999cfe5dbe4070a40f461e1157bf8bb437f90a9b
Rio Justicio <riojusticiof@gmail.com>
2021-09-05 14:44:38 +0700

    Second

---
+++ resources/views/user/layout/template.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Appland Bootstrap Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="/user/img/favicon.png" rel="icon">
+    <link href="/user/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="/user/vendor/aos/aos.css" rel="stylesheet">
+    <link href="/user/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/user/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/user/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="/user/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="/user/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="/user/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Appland - v4.3.0
+  * Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    @include('user.layout.header')
+
+    @yield('content')
+
+    @include('user.layout.footer')
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="/user/vendor/aos/aos.js"></script>
+    <script src="/user/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/user/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="/user/vendor/php-email-form/validate.js"></script>
+    <script src="/user/vendor/swiper/swiper-bundle.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="/user/js/main.js"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get("/", [UserController::class, 'index']);
+Route::get("/adminpages", [AdminController::class, 'index']);
