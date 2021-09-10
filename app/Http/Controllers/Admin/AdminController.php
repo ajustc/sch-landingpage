5196633ca0701150dde56703e501373508fc207e
Rio Justicio <riojusticiof@gmail.com>
2021-09-11 00:43:50 +0700

    admin

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        echo "<pre>";
-        print_r(Cookie::get('sch'));
-        echo "</pre>";
+        $data = [];
 
-        var_dump($_COOKIE);
+        return view('admin.index', $data);
     }
 }
